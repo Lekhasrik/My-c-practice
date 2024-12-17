8c23637d71efdd95b2f6ff3bd4d60e50d0fe4a2c
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:25:47 +0530

    Create circateStadium.c

---
+++ circateStadium.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    //fill the code
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int c=(a+a)+(b+b);
+    int d=a*b;
+    printf("%d\n",c);
+    printf("%d",d);
+    
+    return 0;
+}
