c291533fa0686a697679672afdef2e269337ddda
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:30:40 +0530

    Create fourMuskiters.c

---
+++ fourMuskiters.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    //fill the code
+    float a,b,c,d,e,f;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    scanf("%f",&e);
+    scanf("%f",&f);
+    float fir=(a+c+e)/3;
+    float sec=(b+d+f)/3;
+    printf("%.1f\n",fir);
+    printf("%.1f",sec);
+    return 0;
+    
+}
