a7c81ee415050c79ab1fb3d1af5c20aa86e0f1f7
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:23:55 +0530

    Create tsunamiReport.c

---
+++ tsunamiReport.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    //fill the code
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("1)Dead : %d\n",a);
+    printf("2)Injured : %d\n",b);
+    printf("3)Safe : %d\n",c);
+    printf("please help the people who are suffering!!!");
+    return 0;
+}
