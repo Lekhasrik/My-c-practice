5a904aa4dc1633efd895dd3d6f3d18b9f57d4c31
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:24:43 +0530

    Create arithmaticOperator.c

---
+++ arithmaticOperator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    // fill the code
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a%b);
+    printf("%d\n",a/b);
+    
+    return 0;
+}
