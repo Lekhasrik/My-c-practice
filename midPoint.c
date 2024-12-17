88c691d942e8714b8b8d028630e1081d67deb2b9
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:27:54 +0530

    Create midPoint.c

---
+++ midPoint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    // fill the code
+    float a,b,c,d;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    float e=(a+c)/2;
+    float f=(b+d)/2;
+    printf("%.1f\n",e);
+    printf("%.1f",f);
+    return 0;
+}
