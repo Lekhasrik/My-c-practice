c6954db63afbea403b4cbd195cee733503323293
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:22:09 +0530

    Create roundoff.c

---
+++ roundoff.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //fill the code
+    float a;
+    scanf("%f",&a);
+    printf("%d\n",(int)a);
+    printf("%.1f\n",ceil(a));
+    printf("%.1f\n",floor(a));
+    
+    return 0;
+}
