6e81047ed403fad12fd556fe4ee68735bea4ef66
Lekhasrik <lekhasrikumaresan@gmail.com>
2024-12-17 16:18:41 +0530

    Create decimalSpace.c

---
+++ decimalSpace.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    //fill the code
+    float a;
+    scanf("%f",&a);
+    printf("%f\n",a);
+    printf("%.3f\n",a);
+    printf("%.2f\n",a);
+    printf("%.1f\n",a);
+    return 0;
+}
